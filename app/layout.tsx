b06780605a3c937b62ed9afcca23156c3cf44b29
memorandumtk <memorandum876@outlook.com>
2023-11-18 01:49:35 -0800

    after chapter 5 finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { inter } from '@/app/ui/fonts'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
